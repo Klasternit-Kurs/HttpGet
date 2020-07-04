4d0c4534f52bd9572f1bbe716838de9decf60e73
Dragan Pepic <d.pepic@hotmail.com>
2020-07-04 16:56:04 +0200

    Add project files.

---
+++ HttpGet/Server/Controllers/TestGet.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HttpGet.Server.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class TestGet : ControllerBase
+	{
+		[HttpGet]
+		public IEnumerable<string> OvoJeGet()
+		{
+			List<string> lst = new List<string>();
+			lst.Add("Jen");
+			lst.Add("Dva");
+			lst.Add("Tri");
+
+			return lst;
+		}
+	}
+}
